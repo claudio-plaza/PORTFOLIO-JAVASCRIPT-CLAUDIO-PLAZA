b0de8a01a218fa8d7fb5b76d50ac7ee09ac39c9f
Claudio Plaza <claudiogabrielplaza@hotmail.com>
2025-10-27 10:52:58 -0300

    agenda

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ console.log("el resultado de la operacion " + operacion + " entre " + numero1 + 
 
 ///clase 2  Adivina el numero 
 
-////temas condicionales, operadores logicos y comparacion (==, <, >, <=, >=, !=)
-alert("Bienvenido al juego de Adivina el numero! Tienes 3 intentos para adivinar el numero secreto entre 1 y 10.");
+////temas condicionales, operadores logicos y comparacion (==, <, >, <=, >=, !=) 
+/*alert("Bienvenido al juego de Adivina el numero! Tienes 3 intentos para adivinar el numero secreto entre 1 y 10.");
 const numeroSecreto = Math.floor(Math.random() * 10) + 1; /// numero entre en 1 y 10 
 let intentos = 3;
 let adivinadas = false;
@@ -52,6 +52,66 @@ while (intentos > 0 && !adivinadas){
 }
 if (!adivinadas) {
     alert(" :(lo siento, el numero secreto era " + numeroSecreto);
-}
+}*/
 
 /// permita que el usuario elija el rango
+
+
+
+
+
+
+///ej 6 
+
+let agenda = [];
+//funciona pára agendar contactos 
+
+function agregarContacto(nombre, telefono, email) {
+    const contacto = {
+        nombre,
+        telefono,
+        email
+    };
+    agenda.push(contacto);
+    console.log(`Contacto agregado: ${nombre}`);
+}
+
+
+///funcnion para mostrar contactos 
+
+
+function mostrarAgenda(){
+    console.log("lista de contactos");
+    agenda.forEach(({nombre, telefono, email}, i) =>{
+        console.log(`${i +1}. ${nombre} - ${telefono} - ${email}`);
+    });
+}
+
+//// funcion para buscar contacto 
+
+function buscarContacto(nombre) {
+    const contacto = agenda.find(
+        (c) => c.nombre.tolowerCase() === nombre.tolowerCase()
+    );
+    if (contacto) {
+        console.log(`encontrado : ${contacto.nombre} - ${contacto.telefono}`);
+    } else {
+        console.log("contacto no encontrado");
+    }
+};
+
+///funccion para esportar agenda a JSON
+
+function  exportarAgenda() {
+    const json = JSON.stringify(agenda, null, 2);
+    console.log("Agenda exportada en formato JSON:");
+}
+
+// programa principal 
+
+agregarContacto("ana", "123456789", "ana@example.com");
+agregarContacto("juan", "987654321", "juan@example.com");
+mostrarAgenda();
+
+buscarContacto("ana");
+exportarAgenda();
